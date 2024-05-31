fd275d50511aaab529de96fd5807da73d5cff9bd
unknown <GomezBrian>
2024-05-31 14:26:08 -0300

    Simulacion de parcial terminada

--- SimulacroDeParcial3/js/function.js
+++ SimulacroDeParcial3/js/function.js
@@ -21,15 +21,11 @@ function getCharacter(){
         for (let i = 0; i < 10; i++) {
             
             if (data[i].gender === "female") {
-                
                 contadorFemale ++;
-                let datosFemale = [data[i].name.first]
-                console.log(datosFemale[i])
+                datosFemale.push(data[i].name.first + " " + data[i].name.last)
             } else {
-
                 contadorMale ++;
-                let datosMale = [data[i].name.first]
-                console.log(datosMale[i])
+                datosMale.push(data[i].name.first + " " + data[i].name.last)
             }
 
         }
@@ -37,17 +33,32 @@ function getCharacter(){
         console.log("La cantidad de male son: " + contadorMale)
         console.log("La cantidad de female son: " + contadorFemale)
 
-        for (let i = 0; i < data.length; i++) {
-            for (let j = 0; j < datosFemale.length; j++) {
+        const pjFemale = document.getElementById("pjFemale")
+        const pjMale = document.getElementById("pjMale")
 
-                if (data[i].name.first === datosFemale[j].name.first) {
-                    pjFemale.innerHTML = `
-                        <p> Nombre y apellido: ${datosFemale[i]} </p>
-                        `
-                }
-                
-            }
+        pjFemale.innerHTML = `
+            <h3> Female </h3>
+            `;
+
+        for (let i = 1; i < datosFemale.length; i++) {
             
+            pjFemale.innerHTML += `
+            <p> Nombre y apellido: ${datosFemale[i]}
+            `;
         }
+
+
+        pjMale.innerHTML = `
+        <h3> Male </h3>
+        `;
+
+        for (let i = 1; i < datosMale.length; i++) {
+            
+            pjMale.innerHTML += `
+            <p> Nombre y apellido: ${datosMale[i]}
+            `;
+        }
+        
     })
+
 }
