4482d52d91b112e2118e5ebdeb9e86cf766bbba6
GomezBrianJoelAgustin <agustin.g2400@gmail.com>
2024-06-06 21:31:05 -0300

    Simulacion de parcial terminada

---
+++ function.js
@@ -0,0 +1,66 @@
+/*
+Traer 
+3 personajes de rick and morty y 3 de random user.
+De randomuser traer la persona de mayor edad y mostrarlo.
+De rick and morty traer alfabeticamente con la primer letra del nombre y mostrarlo.
+
+En un objeto guardar datos de dos objetos distintos.
+Name RM
+edad RU
+*/
+
+var mayorRM = [];
+var IdMayorRM = 0;
+
+var mayorRU = 0;
+var IdMayorRU = 0;
+
+function getCharacter() {
+
+    fetch(`https://rickandmortyapi.com/api/character/1,183,2`)
+    .then(res => res.json())
+    .then(dataRM => {
+
+        console.log(dataRM)        
+
+        fetch(`https://randomuser.me/api/?results=3`)
+        .then(resp => resp.json())
+        .then(dataRU => {
+
+            console.log(dataRU)
+            
+
+            for (let i = 0; i < 3; i++) {
+
+                if (dataRU.results[i].dob.age >= mayorRU) {
+
+                    mayorRU = dataRU.results[i].dob.age;
+                    
+                }
+            }
+
+            for (let i = 0; i < 3; i++) {
+                
+                mayorRM.push(dataRM[i].name)
+                
+                console.log("Los nombres son: " + mayorRM)
+
+                mayorRM.sort();
+                
+            }
+
+            console.log("El mayor de Rickandmorty es: " + mayorRM[0])
+
+            console.log("El mayor de randomuser es: " + mayorRU)
+
+            pjRM.innerHTML = `
+            <h1> El mayor de Rickandmorty es: ${mayorRM[0]}
+            `
+            pjRU.innerHTML = `
+            <h1> El mayor de RandomUser es: ${mayorRU}
+            `
+
+        })
+    })
+    
+}
